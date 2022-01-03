d473c3a86a8b7e7cc4f7680813b460e39eb582b3
iman nemati <imun22@gmmail.com>
2022-01-03 19:28:55 +0330

    Add MellatGatewayOptions for wrapping nesesary data for Melat service in one class

---
+++ src/Paymun.Core/Models/GatewayOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Paymun.Core.Models
+{
+    /// <summary>
+    /// Class to keep Gateway options such as MerchantId, Username, Password, etc...
+    /// </summary>
+    public abstract class GatewayOptions
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/Paymun.Gateway.Mellat/Models/MellatGatewayOptions.cs
@@ -0,0 +1,22 @@
+﻿using Paymun.Core.Models;
+
+namespace Paymun.Gateway.Mellat {
+
+    /// <summary>
+    /// Options for Mellat payment gateway services
+    /// </summary>
+    public class MellatGatewayOptions : GatewayOptions {
+
+        public long TerminalId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Determines that the Requests will sent to test url of Mellat service or not.
+        /// </summary>
+        public bool TestTerminal { get; set; }
+    }
+
+}
